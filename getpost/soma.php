9ef03f3dcdd691654dfcd692a713a09c606ab756
Paloma <palomaleandrosenac@gmail.com>
2026-06-25 11:41:12 -0300

    .

---
+++ getpost/soma.php
@@ -0,0 +1,6 @@
+<?php
+$num1 = $_POST['num1'];
+$num2 = $_POST['num2'];
+$soma = $num1 + $num2; 
+
+echo "A soma dos números é: $soma"; 
---
+++ soma.php
@@ -0,0 +1,5 @@
+<?php 
+$soma = 10 + 5;
+echo "A soma de 10 e 5 é: $soma";
+
+?>
